3cbafcb04e9b5eff09f24cf4c6bda5ce889e2441
Seunmatthew <olanrewajuseun17@gmail.com>
2022-09-08 16:19:26 -0700

    Starting with C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,5 +6,5 @@ int main(void)
 	printf("Size of long int: %ld byte(s)",sizeof(long int));
 	printf("Size of a long long int: %ld byte(s)\n",sizeof(long long int));
 	printf("Size of a float: &ld byte(s)\n",sizeof(float));
-	return 0;
+	return (0);
 }
