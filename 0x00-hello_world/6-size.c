2761180b4dc2c56be6b3d0690671000b81db7745
Seunmatthew <olanrewajuseun17@gmail.com>
2022-09-08 16:02:49 -0700

    Starting with C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("size of char: %ld byte(s)\n", sizeof(char;))
+	printf("Size if int: %ld byte(s)\n",sizeof(int));
+	printf("Size of long int: %ld byte(s)",sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n",sizeof(long long int));
+	printf("Size of a float: &ld byte(s)\n",sizeof(float));
+	return 0;
+}
