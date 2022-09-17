1e174d348463f126c3f0d119dfc8ea662d698ec8
Seunmatthew <olanrewajuseun17@gmail.com>
2022-09-16 18:47:21 -0700

    task five

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,19 @@
 void more_numbers(void)
 {
 	int i, j;
-	for(i = 0; i <=9; i++)
+
+	for (i = 0; i <= 9; i++)
+
 	{
-		for ( j = 0; j <=14; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j=>10)
+			if (j >= 10)
 			{
-				_putchar((i/10) + '0');
+				_putchar ((i / 10) + ' 0 ');
 			}
-				_putchar((i%10) + '0');
+				_putchar ((i % 10) + ' 0 ');
 		}
-			_putchar('\n');
+			_putchar (' \n ');
 	}
 }
 
