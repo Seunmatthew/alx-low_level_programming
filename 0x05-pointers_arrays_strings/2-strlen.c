26760b5bb5417a7ecfe3a6f391809910a4eb5ca9
Seunmatthew <olanrewajuseun17@gmail.com>
2022-09-20 14:25:23 -0700

    task one

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+1. #include "main.h"
+
+/**
+ * Return lens- Return length of string
+ *
+ *@a: pointer argument 1
+ *@b: pointer argument 2
+ */
+ int _strlen(char *s)
+{
+	int kont;
+	for (kont =0; *s != '\0\'; ++s)
+		count ++;
+	return (count);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 int _putchar(char c);
 void reset_to_98(int *n);
+int _strlen(char *s);
+
+
 
 #endif
