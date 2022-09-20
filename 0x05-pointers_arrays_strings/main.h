70d9db8016999083fccc49aeda284c22eb6367da
Seunmatthew <olanrewajuseun17@gmail.com>
2022-09-20 15:46:28 -0700

    task four

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,8 +10,9 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, n);
+void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
 
 
 #endif
