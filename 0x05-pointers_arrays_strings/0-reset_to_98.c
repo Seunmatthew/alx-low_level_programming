0ffe95613a2cf01f6051e88e66954f5149356f74
Seunmatthew <olanrewajuseun17@gmail.com>
2022-09-20 04:51:12 -0700

    hot fix

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+reset_to_98(int *n);
+{
+	int n;
+	int *n;
+	n = 402;
+	p = &n;
+	{
+		_putchar("'n'= %d\n" n);
+	}
+		*n  = 98
+
+	{	_putchar("n=%d\n" n);}
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+learn pointers array and string
