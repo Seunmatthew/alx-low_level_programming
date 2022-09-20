75399a42f4f0ab388befcf59867f0103d17e9eed
Seunmatthew <olanrewajuseun17@gmail.com>
2022-09-20 14:58:12 -0700

    task three

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,12 +4,14 @@
  * swap_int - swaps the values pointed to by arguments
  *
  *@str: printing a string
+ *
+ *@_puts: printing a string
  */
 void _puts(char *str)
 {
-	while(*str != '\0')
+	while (*str != '\0')
 	{
-		_putchar(*str +0);
+		_putchar(*str + 0);
 		str++;
 	}
 		_putchar('\n');
