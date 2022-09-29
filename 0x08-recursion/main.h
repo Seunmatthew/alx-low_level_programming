f5daf31de457ea23876aea3877159f8ef00c8858
Seunmatthew <olanrewajuseun17@gmail.com>
2022-09-29 04:12:01 -0700

    recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H                                                         
+#define MAIN_H                                                         
+                                                                       
+/**                                                                    
+ * File: main.h                                                                   
+ */                                                                    
+                                                                       
+#include <stddef.h>                                                    
+                                                                       
+int _putchar(char c);
+#endif
